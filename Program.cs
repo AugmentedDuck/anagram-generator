47de0c184d970ec9444dd9a585fc9d304138f701
AugmentedDuck <anders.m.matzen@gmail.com>
2023-08-09 12:19:50 +0200

    Single word anagram generation added

--- Program.cs
+++ Program.cs
@@ -9,13 +9,75 @@
 
             string input = Console.ReadLine();
             string fixedInput = NormalizeString(input);
-            Console.WriteLine(fixedInput);
 
-            string stringOfWords = ImportWords();
-            string[] words = stringOfWords.Split("\n");
+            string[] words = ImportWords();
+            
+            string[] outputWords = Array.Empty<string>();
+            int indexOfOutput = 0;
+
+            foreach (string word in words)
+            {
+                char[] tempInput = new char[fixedInput.Length];
+
+                for (int i = 0; i < tempInput.Length; i++)
+                {
+                    tempInput[i] = fixedInput[i];
+                }
+
+                if (word.Length > tempInput.Length)
+                {
+                    continue;
+                }
+
+                int nonMatchingLetters = 0;
+
+                foreach(char letter in word)
+                {
+                    int indexInWord = 0;
+                    foreach (char checkLetter in tempInput)
+                    {
+                        if (letter == checkLetter)
+                        {
+                            tempInput[indexInWord] = ' ';
+                            break;
+                        }
+
+                        indexInWord++;
+
+                        if (indexInWord == tempInput.Length)
+                        {
+                            nonMatchingLetters++;
+                        }
+                    }
+                }
+
+                if (nonMatchingLetters != 0)
+                {
+                    continue;
+                }
+
+                Array.Resize(ref outputWords, indexOfOutput + 1);
+
+                string remainingLetters = "";
+
+                foreach (char letter in tempInput)
+                {
+                    remainingLetters += letter;
+                }
+
+                outputWords[indexOfOutput] = word + " - " + NormalizeString(remainingLetters);
+                indexOfOutput++;
+            }
+
+            foreach (string word in outputWords)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine(outputWords.Length);
         }
 
-        static string ImportWords()
+        static string[] ImportWords()
         {
             string allWordsInString = "";
             string directory = "dictionaries";
@@ -30,7 +92,9 @@
                 }
             }
 
-            return allWordsInString;
+            string[] words = allWordsInString.Split("\n");
+
+            return words;
         }
 
         static string NormalizeString(string inputString)
