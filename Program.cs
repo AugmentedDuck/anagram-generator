043d0e8c81a02e08b5a1344e63dda8327a83a73c
AugmentedDuck <anders.m.matzen@gmail.com>
2023-08-09 11:13:15 +0200

    Updated formatting

--- Program.cs
+++ Program.cs
@@ -1,45 +1,41 @@
-﻿using System.IO;
-using System.Linq;
-
-namespace AnagramGenerator
+﻿namespace AnagramGenerator
 {
-  class Program
-  {
-    static void Main()
+    class Program
     {
-      Console.WriteLine("Welcome to the anagram generator");
-      Console.WriteLine("Please write the input word(s):");
-
-      string input = Console.ReadLine();
-
-      string fixedInput = NormalizeString(input);
-      Console.WriteLine(fixedInput);
-
-      string stringOfWords = ImportWords();
-      string[] words = stringOfWords.Split("\n");
-    }
-
-    static string ImportWords()
-    {
-      string allWordsInString = "";
-      string directory = "dictionaries";
-      
-      string[] fileEntries = Directory.GetFiles(directory);
-      foreach (string fileName in fileEntries)
-      {
-        if (fileName.ToLower().EndsWith(".txt"))
+        static void Main()
         {
-          Console.WriteLine("Importing words from the " + fileName.Remove(0, directory.Length + 1) + " dictionary");
-          allWordsInString += File.ReadAllText(fileName);
+            Console.WriteLine("Welcome to the anagram generator");
+            Console.WriteLine("Please write the input word(s):");
+
+            string input = Console.ReadLine();
+            string fixedInput = NormalizeString(input);
+            Console.WriteLine(fixedInput);
+
+            string stringOfWords = ImportWords();
+            string[] words = stringOfWords.Split("\n");
         }
-      }
 
-      return allWordsInString;
-    }
+        static string ImportWords()
+        {
+            string allWordsInString = "";
+            string directory = "dictionaries";
 
-    static string NormalizeString(string inputString)
-    {
-      return String.Concat(inputString.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+            string[] fileEntries = Directory.GetFiles(directory);
+            foreach (string fileName in fileEntries)
+            {
+                if (fileName.ToLower().EndsWith(".txt"))
+                {
+                    Console.WriteLine("Importing words from the " + fileName.Remove(0, directory.Length + 1) + " dictionary");
+                    allWordsInString += File.ReadAllText(fileName);
+                }
+            }
+
+            return allWordsInString;
+        }
+
+        static string NormalizeString(string inputString)
+        {
+            return String.Concat(inputString.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+        }
     }
-  }
 }
