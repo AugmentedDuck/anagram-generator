e5a62b3899cecb5df5a98f6904087cb1b5f691a3
AugmentedDuck <anders.m.matzen@gmail.com>
2023-08-09 19:55:43 +0200

    Now works with GUI

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBox1.Location = new System.Drawing.Point(20, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(275, 40);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Include English words";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.InEnglish);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBox2.Location = new System.Drawing.Point(20, 60);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(275, 40);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Include Danish words";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.IsDanish);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBox3.Location = new System.Drawing.Point(20, 100);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(275, 40);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Include User words";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.UseUserWords);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBox4.Location = new System.Drawing.Point(534, 20);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(325, 40);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Include single letter words";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.IsSingleWord);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(480, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(387, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate Anagrams";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GenerateAnagrams);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(475, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Results are saved in \"Results\" directory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox1.Location = new System.Drawing.Point(12, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 39);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Input string";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(879, 524);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(750, 255);
+            this.Name = "Form1";
+            this.Text = "Anagram Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using AnagramGenerator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        readonly Core core = new Core();
+
+        bool isEnglish = false;
+        bool isDanish = false;
+        bool useUserWords = false;
+        bool useSingleWords = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InEnglish(object sender, EventArgs e)
+        {
+            isEnglish = !isEnglish;
+        }
+
+        private void UseUserWords(object sender, EventArgs e)
+        {
+            useUserWords = !useUserWords;
+        }
+
+        private void IsDanish(object sender, EventArgs e)
+        {
+            isDanish = !isDanish;
+        }
+
+        private void IsSingleWord(object sender, EventArgs e)
+        {
+            useSingleWords = !useSingleWords;
+        }
+
+        private void GenerateAnagrams(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+
+            string[] words = core.ImportWords(isEnglish, isDanish, useUserWords);
+            string[] anagrams = core.AnagramFinder(core.NormalizeString(inputString), words, useSingleWords);
+
+            string anagramString = "";
+
+            foreach (string word in anagrams) 
+            {
+                anagramString += word + "\n";
+            }
+
+            string outputFile = $"Results/'{inputString}'-anagrams.txt";
+
+            Directory.CreateDirectory("Results");
+            File.WriteAllText(outputFile, anagramString);
+        }
+    }
+}
